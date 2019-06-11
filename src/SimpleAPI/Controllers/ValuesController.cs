626904ecb3a2e9e2cfe2390390452bc07e942bfb
Vishal <vishsdn@outlook.com>
2019-06-11 13:12:37 +0530

    Broke code1

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Meenakshi Gupta";
+            return "My bad";
         }
 
         // POST api/values
